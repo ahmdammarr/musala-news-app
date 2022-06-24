c803cc354ca2de0b3cd34c9d2495016370f1aeef
ammarr <ahmedmahmoud9597@gmail.com>
2022-06-25 00:38:26 +0300

    refactor/ use one shared params

--- src/api/getNews/getNewsApi.ts
+++ src/api/getNews/getNewsApi.ts
@@ -3,28 +3,26 @@ import { to } from "await-to-js";
 import { getCurrentLanguage } from "localization";
 import { API_TOKEN } from "@env";
 import { baseURI } from "api/baseURI";
+
 import { ELanguages } from "shared/enums";
 
+const selectedLang = getCurrentLanguage();
+const country = selectedLang === ELanguages.ar ? "eg" : "us";
+const _params = {
+  access_key: API_TOKEN,
+  countries: country,
+  language: selectedLang,
+};
+
 export const getNewsApi = async (offset: number) => {
-  const selectedLang = getCurrentLanguage();
-  const country = selectedLang === ELanguages.ar ? "eg" : "us";
-  const params = {
-    access_key: API_TOKEN,
-    countries: country,
-    language: selectedLang,
-    offset,
-  };
+  const params = { ..._params, offset };
   const [error, response] = await to(axios.get(baseURI, { params }));
   return { error, news: response?.data?.data };
 };
 
 export const getSearchNewsApi = async (offset: number, keyword?: string) => {
-  const selectedLang = getCurrentLanguage();
-  const country = selectedLang === ELanguages.ar ? "eg" : "us";
   const params = {
-    access_key: API_TOKEN,
-    countries: country,
-    language: selectedLang,
+    ..._params,
     offset,
     keywords: keyword || "",
   };
